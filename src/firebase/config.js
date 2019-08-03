12430551713705cb8d997407d058c361a08ad94b
Gayrat Vlasov <33682052+Ulibka68@users.noreply.github.com>
2019-08-03 13:15:58 +0300

    Начальная конфигурация для Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+// learnweb
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAXY-tJIg6oFJxMacejrEFC2PLSyAoVRKY",
+    authDomain: "learn-3d592.firebaseapp.com",
+    databaseURL: "https://learn-3d592.firebaseio.com",
+    projectId: "learn-3d592",
+    storageBucket: "learn-3d592.appspot.com",
+    messagingSenderId: "695109226338",
+    appId: "1:695109226338:web:2956d7be408ed388"
+};
+
+export default firebaseConfig;
---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+import regeneratorRuntime from "regenerator-runtime";
+
+console.log('main1');
+
+import {firebase} from '~f/firebase';
+
